1a66b63dd64acce56970cfe2f2c0125a00fc75e1
christophszcz <christoph.szczechowicz@gmail.com>
2017-04-24 15:07:51 -0400

    Add a zero digit to seconds < 10.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var timer = {
+var Timer = {
 	minutes: 25,
 	seconds: 60
 };
@@ -9,11 +9,18 @@ var timer = {
 // }
 
 var clock = document.getElementById('clock');
-clock.innerHTML = ("<p>" + timer.seconds + "</p>");
+clock.innerHTML = ("<p>" + Timer.seconds + "</p>");
+
+function addDigit(num) {
+  return (num < 10) ? '0' + num.toString() : num.toString();
+}
 
 function secondsCountDown(){
-	timer.seconds -- ;
-	clock.innerHTML = ("<p>" + timer.seconds + "</p>");
+	if (Timer.seconds <= 0){
+		Timer.seconds = 60;
+	}
+	Timer.seconds -- ;
+	clock.innerHTML = ("<p>" + addDigit(Timer.seconds) + "</p>");
 }
 
 setInterval(secondsCountDown, 1000);
