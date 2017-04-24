f7b0370d8ffabdd054e677c70275785bc638a97e
christophszcz <christoph.szczechowicz@gmail.com>
2017-04-24 13:55:31 -0400

    Create a seconds countdown.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-$(document).ready(function(){
+var timer = {
+	minutes: 25,
+	seconds: 60
+};
 
-});
+// while (timer.minutes > 0) {
+// 	function 
 
+// }
+
+var clock = document.getElementById('clock');
+clock.innerHTML = ("<p>" + timer.seconds + "</p>");
+
+function secondsCountDown(){
+	timer.seconds -- ;
+	clock.innerHTML = ("<p>" + timer.seconds + "</p>");
+}
+
+setInterval(secondsCountDown, 1000);
  
