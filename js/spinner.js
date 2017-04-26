78c2545e046de9ac410c607e8e16881036a4beb0
christophszcz <christoph.szczechowicz@gmail.com>
2017-04-26 13:41:39 -0400

    Remove negative work session selection.

--- js/spinner.js
+++ js/spinner.js
@@ -4,23 +4,22 @@ $(function() {
   var spinner = $( "#spinner" ).spinner();
   spinner.spinner( "value", 25 );
 
-  $( "#disable" ).on( "click", function() {
-    if ( spinner.spinner( "option", "disabled" ) ) {
-      spinner.spinner( "enable" );
-    } else {
-      spinner.spinner( "disable" );
-    }
-  });
-  $( "#destroy" ).on( "click", function() {
-    if ( spinner.spinner( "instance" ) ) {
-      spinner.spinner( "destroy" );
-    } else {
-      spinner.spinner();
-    }
-  });
-  $( "#getvalue" ).on( "click", function() {
-    alert( spinner.spinner( "value" ) );
-  });
-
-  $( "button" ).button();
+  // $( "#disable" ).on( "click", function() {
+  //   if ( spinner.spinner( "option", "disabled" ) ) {
+  //     spinner.spinner( "enable" );
+  //   } else {
+  //     spinner.spinner( "disable" );
+  //   }
+  // });
+  // $( "#destroy" ).on( "click", function() {
+  //   if ( spinner.spinner( "instance" ) ) {
+  //     spinner.spinner( "destroy" );
+  //   } else {
+  //     spinner.spinner();
+  //   }
+  // });
+  // $( "#getvalue" ).on( "click", function() {
+  //   alert( spinner.spinner( "value" ) );
+  // });
+  // $( "button" ).button();
 });
