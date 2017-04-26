37f0a623c9be9bffe17cf5421cc44a65bf6aad20
christophszcz <christoph.szczechowicz@gmail.com>
2017-04-26 13:57:39 -0400

    Make it not possible start the timer with without positive integer values.

--- js/startPage.js
+++ js/startPage.js
@@ -2,6 +2,12 @@ var start = document.getElementById('start-page');
 spinner = $( "#spinner" ).spinner();
 
 start.addEventListener('click', function(){
+	if (spinner.spinner( "value" ) < 0 || spinner.spinner( "value" ) === null ){
+		spinner.spinner( "value", 25 );
+		alert('Please enter a valid time value.');
+	}
 	Timer.minutes = spinner.spinner( "value" );
 	minutes.innerHTML = ("<p>" + Timer.minutes + "</p>"); 
 });
+
+
